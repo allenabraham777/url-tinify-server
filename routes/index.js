c30da2db3d8000479fcba0d497569c09cd5d1651
Allen K Abraham <allen@theabstraction.co>
2020-07-25 17:33:16 +0530

    Redirection to client page added

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,18 @@ const Url = require('../models/url')
 module.exports.register = (server, options) => {
   server.route({
     method: 'GET',
+    path: '/',
+    config: {
+      tags: ['api', 'index'],
+      description: 'Redirect to Home Page',
+    },
+    handler: async (request, h) => {
+      return h.redirect('https://client.cutcut.cf')
+    }
+  })
+
+  server.route({
+    method: 'GET',
     path: '/{shortUrl}',
     config: {
       tags: ['api', 'index'],
