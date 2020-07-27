f741a2ece3c78926196dcf1dd3ed053c853350be
Allen K Abraham <allen@theabstraction.co>
2020-07-28 00:27:02 +0530

    Removed unwanted commands

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const createToken = (user) => {
-  return await jwt.sign({ id: user._id, email: user.email, name: user.name }, process.env.SECRET);
+  return jwt.sign({ id: user._id, email: user.email, name: user.name }, process.env.SECRET);
 }
 
 module.exports = {createToken}
